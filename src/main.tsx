aa45f7b075966b9f2934b10995cef9b9631dbb32
Acaibird <durbannhawkey42@gmail.com>
2024-06-17 11:02:32 +0800

    feat:set login router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import Login from "@/components/login.tsx";
 import './index.css'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: Login(),
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>,
 )
