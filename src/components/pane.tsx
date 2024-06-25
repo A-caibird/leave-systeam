dd87be6bb72766bd3b3471d6c0ef13d25e0ee720
Acaibird <durbannhawkey42@gmail.com>
2024-06-25 14:17:55 +0800

    feat: home pane layout

--- src/components/pane.tsx
+++ src/components/pane.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Calendar } from 'antd';
-import type { CalendarProps } from 'antd';
-import type { Dayjs } from 'dayjs';
+
 
 const Pane: React.FC = () => {
-    const onPanelChange = (value: Dayjs, mode: CalendarProps<Dayjs>['mode']) => {
-        console.log(value.format('YYYY-MM-DD'), mode);
-    };
+    return (
+        <div className="grid grid-rows-[1fr_2fr] z-1 w-full h-full">
+            <div className="bg-red-500  grid grid-cols-[1fr_2fr]">
+                <div className='bg-gray-700'>
 
-    return <Calendar onPanelChange={onPanelChange} />;
+                </div>
+                <div className="bg-amber-200 grid grid-cols-2">
+                    <div className="bg-green-100"></div>
+                    <div className="bg-green-200"></div>
+                </div>
+            </div>
+            <div className="bg-blue-400 grid grid-rows-[1fr_3fr]">
+                <div className="bg-amber-700"></div>
+                <div className="bg-green-200">
+
+                </div>
+            </div>
+        </div>
+    )
 };
 
 export default Pane;
