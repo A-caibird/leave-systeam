d9a3ae36d462c43476c6fdfb85fd8cb6d2c57377
Acaibird <durbannhawkey42@gmail.com>
2024-06-26 20:47:12 +0800

    feat: set zh/cn for all component from ant design

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { App } from "@/App";
+import { ConfigProvider } from "antd"
+import zhCN from "antd/lib/locale/zh_CN"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+        <ConfigProvider locale={zhCN}>
+            <App />
+        </ConfigProvider>,
     </React.StrictMode>,
 )
