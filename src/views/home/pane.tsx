34c567561e72db4d0abcd8cab9b5991a79bbf611
Acaibird <durbannhawkey42@gmail.com>
2024-06-27 02:49:22 +0800

    feat: visual  chart in pane

--- src/views/home/pane.tsx
+++ src/views/home/pane.tsx
@@ -1,11 +1,48 @@
 import React from 'react';
+import { BarList } from '@tremor/react';
 
+const datahero1 = [
+    { name: '已通过', value: 456 },
+    { name: '待批准', value: 351 },
+    { name: '已拒绝', value: 51 },
+];
 
+import { DonutChart } from '@tremor/react';
+
+const datahero2 = [
+    {
+        name: 'Noche Holding AG',
+        value: 9800,
+    },
+    {
+        name: 'Rain Drop AG',
+        value: 4567,
+    },
+    {
+        name: 'Push Rail AG',
+        value: 3908,
+    },
+    {
+        name: 'Flow Steal AG',
+        value: 2400,
+    },
+    {
+        name: 'Tiny Loop Inc.',
+        value: 2174,
+    },
+    {
+        name: 'Anton Resorts Holding',
+        value: 1398,
+    },
+];
+
+const dataFormatter = (number: number) =>
+    `$ ${Intl.NumberFormat('us').format(number).toString()}`;
 const Pane: React.FC = () => {
     return (
         <div className="grid grid-rows-[1fr_2fr] z-1 w-full h-full">
             <div className="bg-red-500  grid grid-cols-[1fr_2fr]">
-                <div className='bg-gray-700 min-w-[200px]'>
+                <div className='bg-gray-700 min-w-[300px]'>
                     <div className={"flex items-center justify-around pt-[20px] pb-[5px] border-b-2 border-black"}>
                         <span>
                             <img src={"/"}
@@ -76,9 +113,26 @@ const Pane: React.FC = () => {
                         </table>
                     </div>
                 </div>
-                <div className="bg-amber-200 grid grid-cols-2">
-                    <div className="bg-green-100"></div>
-                    <div className="bg-green-200"></div>
+                <div className="grid gid-cols-2">
+                    <div className="bg-blue-200  dark:bg-gray-500 dark:border-r-black border-r-[1px] col-start-1 col-end-2 flex flex-col justify-center px-[2rem]">
+                        <BarList data={datahero1} className="mx-auto max-w-sm min-w-[20rem]" />
+                    </div>
+                    <div className="bg-blue-100 dark:bg-gray-500 min-w-[300px] col-start-2 col-end-3 ">
+                        <div className="space-y-3 flex flex-col justify-center">
+                            <span
+                                className="text-center block  pt-[2rem] text-black dark:text-amber-50 text-xl dark:text-dark-tremor-content">
+                                请假信息饼图
+                            </span>
+                            <div className="flex justify-center">
+                                <DonutChart
+                                    data={datahero2}
+                                    variant="pie"
+                                    valueFormatter={dataFormatter}
+                                    onValueChange={(v) => console.log(v)}
+                                />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="bg-blue-400 grid grid-rows-[1fr_3fr]">
