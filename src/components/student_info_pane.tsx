2b0232298659b8a18926b9c52f7031b16de7315b
Acaibird <durbannhawkey42@gmail.com>
2024-06-30 02:40:00 +0800

    feat: adjust leave pane  b background  color of dark theme

--- src/components/student_info_pane.tsx
+++ src/components/student_info_pane.tsx
@@ -33,12 +33,12 @@ const dataFormatter = (number: number) =>
     `$ ${Intl.NumberFormat('us').format(number).toString()}`;
 function StudentInfoPane(): React.ReactNode {
     return (
-        <div className="w-full h-full bg-blue-200 grid grid-rows-[1fr_2fr]">
+        <div className="w-full h-full bg-blue-200 dark:bg-slate-500 grid grid-rows-[1fr_2fr]">
             <div className="grid grid-cols-[2fr_1fr] w-full h-full">
                 <div className="!max-h-[400px] ">
                     <StudentBarChart />
                 </div>
-                <div className="bg-green-200 dark:bg-gray-500 min-w-[300px] rounded-lg mr-8">
+                <div className="bg-green-200 dark:bg-[#121826] min-w-[300px] rounded-lg mr-8">
                     <div className="space-y-3 flex flex-col justify-center">
                         <span
                             className="text-center block  pt-[2rem] text-black dark:text-amber-50  text-xl font-bold ">
