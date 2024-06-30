fec1cac556570b83ba1d61eff106ac26a1f7e67e
Acaibird <durbannhawkey42@gmail.com>
2024-07-01 06:12:42 +0800

    feat:添加全局类型

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -13,3 +13,31 @@ declare module "three"{
         far:number
     }
 }
+declare interface Student{
+    "id": "1",
+    "name": "1",
+    "bDate": "2024-07-01T04:03:08",
+    "age": null,
+    "sexy": "1",
+    "password": "1",
+    "phone": "1",
+    "did": "1",
+    "gid": "01",
+    "tid": "1",
+    "role": 0,
+    gname: "1",
+}
+declare type LeaveInfo={
+    "id": 1,
+    "applicantId": "3210621073",
+    "type": "1",
+    "reason": "1",
+    "startTime": "2024-06-29T00:00:00",
+    "endTime": "2024-07-02T00:00:00",
+    "status": 2,
+    "tid": "666666",
+    "approvalTime_t": "2024-06-29T04:16:44.213497",
+    "approvalTime_m": "2024-06-29T04:16:57.382308",
+    "approvalComment_t": "123",
+    "approvalComment_m": null
+}
