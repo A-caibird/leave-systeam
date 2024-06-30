d667d88dd3f0ce99041e30b8a839e91a550badfc
Acaibird <durbannhawkey42@gmail.com>
2024-07-01 06:12:22 +0800

    feat:分页请求

--- src/components/student_info_list.tsx
+++ src/components/student_info_list.tsx
@@ -1,88 +1,60 @@
-import React from 'react'
-import type { PaginationProps } from 'antd';
-import { Pagination } from 'antd';
-const onPageChange: PaginationProps['onChange'] = (pageNumber) => {
-    console.log('Page: ', pageNumber);
-}
-import type { CascaderProps } from 'antd';
-import { Cascader } from 'antd';
-
-interface Option {
-    value: string;
-    label: string;
-    children?: Option[];
-}
-
-const options: Option[] = [
-    {
-        value: 'zhejiang',
-        label: '2020级',
-        children: [
-            {
-                value: '201',
-                label: '计算机科学与技术201',
-            }, {
-                value: '202',
-                label: '计算机科学与技术202',
-            },
-        ],
-    },
-    {
-        value: 'jiangsu',
-        label: '2021级',
-        children: [
-            {
-                value: '211',
-                label: '计算机科学与技术211',
-            }, {
-                value: '212',
-                label: '计算机科学与技术212',
-            },
-        ],
-    },
-];
-
-const onChange: CascaderProps<Option>['onChange'] = (value) => {
-    console.log(value);
-};
+import React, {useEffect, useState} from 'react'
+import type {PaginationProps} from 'antd';
+import {Pagination} from 'antd';
+import {Student} from "../../typings/global";
+import Fetch from "@/utils/api/fetch.ts";
 
 export default function StudentInfoList(): React.ReactNode {
-    return (
+    const [students, setStudents] = useState<Student[]>([])
+    const [pageNumber] = useState(1);
+    const [pageSize, setPageSize] = useState(10);
+    useEffect(() => {
+        Fetch(`/api/students?pageNumber=${pageNumber}&&pageSize=${pageSize}`, {}).then(async (res) => {
+            setStudents(await res.json())
+        }).catch((err)=>{
+            console.log(err);
+        })
 
+    },[])
+
+    const onPageChange: PaginationProps['onChange'] = (pageNumber) => {
+        Fetch(`/api/students?pageSize=${pageSize}&pageNumber=${pageNumber}`).then(async (res) => {
+            setStudents(await res.json())
+        })
+    }
+    function onShowSizeChange(current:number,size:number) {
+        console.log(current,size)
+        setPageSize(size);
+    }
+    return (
         <div className="bg-blue-200 dark:bg-slate-500 w-full absolute">
-            <div>
-                <table className='dark:text-white text-black w-full border-separate border-[0px] *:border-2 [&>:not(thead)]:bg-red-50 [&>:not(thead)]:dark:bg-slate-400'>
+            <div className={"min-h-[318px]"}>
+                <table
+                    className='dark:text-white text-black w-full border-separate border-[0px] *:border-2 [&>:not(thead)]:bg-red-50 [&>:not(thead)]:dark:bg-slate-400'>
                     <thead>
-                        <tr className=' *:bg-slate-400 text-xl'>
-                            <th >学生姓名</th>
-                            <th> <Cascader options={options} onChange={onChange} placeholder="筛选条件" /></th>
-                            <th>学号</th>
-                        </tr>
+                    <tr className=' *:bg-slate-400 text-xl'>
+                        <th>学生姓名</th>
+                        <th>班级</th>
+                        <th>学号</th>
+                    </tr>
                     </thead>
                     <tbody className=' '>
-                        {
-                            Array.from({ length: 10 }, () => (
-                                <tr className='*:text-center   hover:scale-y-150 hover:bg-blue-600  transitio-all duration-100  '>
-                                    <td >
-                                        李安
-                                    </td>
-                                    <td>
-                                        计算机科学与技术212
-                                    </td>
-                                    <td>
-                                        3210613027
-                                    </td>
-                                </tr>
-                            ))
-                        }
+                    {students.map((student) => (
+                        <tr key={student.id} className='*:text-center hover:scale-y-150 hover:bg-blue-600 transition-all duration-100'>
+                            <td>{student.name}</td>
+                            <td>{student.gname}</td>
+                            <td>{student.id}</td>
+                        </tr>
+                    ))}
                     </tbody>
                 </table>
             </div>
-            <div className='flex w-full justify-center mt-[10rem]'>
+            <div className='flex w-full justify-center mt-[40px]'>
                 <div className=' text-center '>
-                    <Pagination showQuickJumper defaultCurrent={2} total={500} pageSize={10} onChange={onPageChange} responsive={true} />
+                    <Pagination showQuickJumper defaultCurrent={2} total={500} pageSize={pageSize} onChange={onPageChange}
+                                responsive={true} onShowSizeChange={onShowSizeChange}/>
                 </div>
             </div>
-        </div >
+        </div>
     )
 }
