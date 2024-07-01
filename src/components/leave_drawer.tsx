cc9e0d0ccab7558d7a15439315e7d2f77bf1159c
Acaibird <durbannhawkey42@gmail.com>
2024-07-01 08:31:04 +0800

    fix:修复学生同意,拒绝请求处理

--- src/components/leave_drawer.tsx
+++ src/components/leave_drawer.tsx
@@ -1,18 +1,68 @@
-import React, { useEffect } from "react"
-import { Drawer } from 'antd';
-export default function LeaveDetailDrawer({ visable, onClose }: { visable: boolean, onClose: () => void }): JSX.Element {
+import React, {useEffect, useState} from "react"
+import {Drawer, Input, message} from 'antd';
+import Fetch from "@/utils/api/fetch.ts";
+
+export default function LeaveDetailDrawer({visable, onClose, index}: {
+    visable: boolean,
+    onClose: () => void,
+    index: number
+}): React.ReactNode {
+    const [leaveInfo, setLeaveInfo] = useState();
+    const [reviewComment, setReviewComment] = useState("");
+    const [need, setNeed] = useState(false);
+    const userInfo = JSON.parse(sessionStorage.getItem("UserInfo") as string)
     useEffect(() => {
         setLoading(true);
+
         // Simple loading mock. You should add cleanup logic in real world.
         const a = setTimeout(() => {
             setLoading(false);
         }, 1000);
+
         return (() => {
             clearTimeout(a)
         })
 
     }, [visable])
+
+    useEffect(() => {
+        Fetch(`/api/leave/${index}`, {
+            method: "GET"
+        }).then(async (resp) => {
+            const data = await resp.json()
+            setLeaveInfo(data);
+            console.log(data)
+            setNeed((data.status !== 2 &&  data.status !== 3)&&(userInfo.role!=0))
+        }).catch(e => {
+            console.log(e)
+        })
+    }, [visable])
+
     const [loading, setLoading] = React.useState<boolean>(true);
+
+    function onReview(id: string) {
+        Fetch(`/api/leave/review_m?id=${leaveInfo.id}&day=${leaveInfo.day}`, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            method: "PUT",
+            body: JSON.stringify({
+                NewStatus: id,
+                ApprovalComment: reviewComment
+            })
+        }).then(async (res) => {
+            if (res.status === 200) {
+                message.info("学生请假请求处理成功!")
+            } else {
+                message.warning("服务器繁忙,请稍后再试!")
+            }
+        })
+    }
+
+    function onInput(e) {
+        setReviewComment(e.target.value)
+    }
+
     return (
         <div>
             <Drawer
@@ -24,9 +74,80 @@ export default function LeaveDetailDrawer({ visable, onClose }: { visable: boole
                 loading={loading}
                 onClose={onClose}
             >
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
+                <div className={"[&>div.flex>span:nth-of-type(1)]:w-[5rem] flex flex-col justify-center gap-y-3 "}>
+                    <div className={"flex gap-x-[4px]"}>
+                        <span>
+                            学生姓名
+                        </span>
+                        <span>
+                            {leaveInfo ? leaveInfo.sname : ""}
+                        </span>
+                    </div>
+                    <div className={"flex gap-x-[4px]"}>
+                        <span>
+                            班级
+                        </span>
+                        <span>
+                            {leaveInfo ? leaveInfo.sname : ""}
+                        </span>
+                    </div>
+                    <div className={"flex gap-x-[4px]"}>
+                        <span>
+                            开始时间
+                        </span>
+                        <span>
+                            {leaveInfo ? leaveInfo.startTime : ""}
+                        </span>
+                    </div>
+                    <div className={"flex gap-x-[4px]"}>
+                        <span>
+                            结束时间
+                        </span>
+                        <span>
+                            {leaveInfo ? leaveInfo.endTime : ""}
+                        </span>
+                    </div>
+                    <div className={"flex gap-x-[4px]"}>
+                        <span>
+                            请假天数
+                        </span>
+                        <span>
+                            {leaveInfo ? leaveInfo.day : ""}
+                        </span>
+                    </div>
+                    <div className={"flex"}>
+                        <span>
+                            请假原因
+                        </span>
+                        <span>
+                            {leaveInfo ? leaveInfo.reason : ""}
+                        </span>
+                    </div>
+                    {
+                        need ? (<div className={"flex gap-x-[4px]"}>
+                            <span>
+                                评语
+                            </span>
+                            <span>
+                                <Input placeholder="请输入您的意见!" onChange={onInput}/>
+                            </span>
+                        </div>) : <div></div>
+                    }
+
+                </div>
+                {
+                    need ?
+                        <div className={"mt-[2rem] flex justify-center gap-x-[4rem]"}>
+                            <span className={"bg-green-400 px-[2rem] py-[2px] rounded-full"}
+                                  onClick={() => onReview("通过")}>
+                                同意
+                            </span>
+                            <span className={"bg-green-400 px-[2rem] py-[2px] rounded-full"}
+                                  onClick={() => onReview("拒绝")}>
+                                拒绝
+                            </span>
+                        </div> : <div></div>
+                }
             </Drawer>
         </div>
     )
--- src/components/leave_list.tsx
+++ src/components/leave_list.tsx
@@ -1,109 +1,141 @@
-import type { PaginationProps } from 'antd';
-import { Pagination, Switch, Tooltip } from 'antd';
-import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
-import { useState } from 'react';
+import type {PaginationProps} from 'antd';
+import {Pagination, Switch, Tooltip} from 'antd';
+import {CheckOutlined, CloseOutlined} from '@ant-design/icons';
+import {useEffect, useState} from 'react';
 import LeaveDetailDrawer from '@/components/leave_drawer';
-const onPageChange: PaginationProps['onChange'] = (pageNumber) => {
-    console.log('Page: ', pageNumber);
-};
+import {LeaveInfo} from "../../typings/global";
+import Fetch from "@/utils/api/fetch.ts";
+
 const LeaveList: React.FC = () => {
     const [checkedList, setCheckedList] = useState<boolean[]>([false, false, false])
     const [visable, setVisable] = useState<boolean>(false)
+    const [leaveInfos, setLeaveInfos] = useState<LeaveInfo[]>([])
+    const [itemIndex, setItemIndex] = useState(0);
+    const [pageNumber] = useState(1);
+    const [pageSize, setPageSize] = useState(10);
+
+    useEffect(() => {
+        Fetch(`/api/leave/student/${"3210621073"}?pageNumber=${pageNumber}&pageSize=${pageSize}`, {}).then(async (res) => {
+            setLeaveInfos(await res.json())
+        }).catch((err)=>{
+            console.log(err);
+        })
+    },[])
+
+    const onPageChange: PaginationProps['onChange'] = (pageNumber) => {
+        Fetch(`/api/leave/student/${"3210621073"}?pageNumber=${pageNumber}&pageSize=${pageSize}`).then(async (res) => {
+            setLeaveInfos(await res.json())
+        })
+    }
+
+    function onShowSizeChange(current:number,size:number) {
+        setPageSize(size);
+    }
+
     function onSwitchChange1(checked: boolean) {
         setCheckedList([checked, false, false])
     }
+
     function onSwitchChange2(checked: boolean) {
         setCheckedList([false, checked, false])
     }
+
     function onSwitchChange3(checked: boolean) {
         setCheckedList([false, false, checked])
     }
-    function onReviewDetail() {
+
+    function onReviewDetail(index: number) {
         setVisable(true)
+        setItemIndex(index)
     }
+
     function onCloseDrawer() {
         setVisable(false)
     }
+
     return (
-        <div className="w-full h-full bg-blue-200 dark:bg-slate-500 relative">
-            <div>
-                <table className='dark:text-white text-black w-full border-separate border-[0px] *:border-2 [&>:not(thead)]:bg-red-50 [&>:not(thead)]:dark:bg-slate-400'>
+        <div className="w-full h-full bg-blue-100 dark:bg-slate-500 relative">
+            <div className={"min-h-[300px]"}>
+                <table
+                    className='dark:text-white text-black w-full border-separate border-[0px] *:border-2 [&>:not(thead)]:bg-red-50 [&>:not(thead)]:dark:bg-slate-400'>
                     <thead>
-                        <tr>
-                            <th className='text-center'>
-                                <span className='flex align-center gap-2 justify-center'>
-                                    <span>
-                                        已通过
-                                    </span>
-                                    <Switch
-                                        checkedChildren={<CheckOutlined />}
-                                        unCheckedChildren={<CloseOutlined />}
-                                        onChange={onSwitchChange1}
-                                        checked={checkedList[0]}
-                                    />
+                    <tr>
+                        <th className='text-center'>
+                            <span className='flex align-center gap-2 justify-center'>
+                                <span>
+                                    已通过
                                 </span>
-                            </th>
-                            <th>
-                                <span className='flex align-center gap-2 justify-center'>
-                                    <span>
-                                        待审核
-                                    </span>
-                                    <Switch
-                                        checkedChildren={<CheckOutlined />}
-                                        unCheckedChildren={<CloseOutlined />}
-                                        onChange={onSwitchChange2}
-                                        checked={checkedList[1]}
-                                    />
+                                <Switch
+                                    checkedChildren={<CheckOutlined/>}
+                                    unCheckedChildren={<CloseOutlined/>}
+                                    onChange={onSwitchChange1}
+                                    checked={checkedList[0]}
+                                />
+                            </span>
+                        </th>
+                        <th>
+                            <span className='flex align-center gap-2 justify-center'>
+                                <span>
+                                    待审核
                                 </span>
-                            </th>
-                            <th className='text-center'>
-                                <span className='flex align-center gap-2 justify-center'>
-                                    <span>
-                                        未通过
-                                    </span>
-                                    <Switch
-                                        checkedChildren={<CheckOutlined />}
-                                        unCheckedChildren={<CloseOutlined />}
-                                        onChange={onSwitchChange3}
-                                        checked={checkedList[2]}
-                                    />
+                                <Switch
+                                    checkedChildren={<CheckOutlined/>}
+                                    unCheckedChildren={<CloseOutlined/>}
+                                    onChange={onSwitchChange2}
+                                    checked={checkedList[1]}
+                                />
+                            </span>
+                        </th>
+                        <th className='text-center'>
+                            <span className='flex align-center gap-2 justify-center'>
+                                <span>
+                                    未通过
                                 </span>
-                            </th>
-                        </tr>
-                        <tr className=' *:bg-slate-400 text-xl'>
-                            <th >申请日期</th>
-                            <th>请假类型</th>
-                            <th>状态</th>
-                        </tr>
+                                <Switch
+                                    checkedChildren={<CheckOutlined/>}
+                                    unCheckedChildren={<CloseOutlined/>}
+                                    onChange={onSwitchChange3}
+                                    checked={checkedList[2]}
+                                />
+                            </span>
+                        </th>
+                    </tr>
+                    <tr className=' *:bg-slate-400 text-xl'>
+                        <th>申请日期</th>
+                        <th>请假类型</th>
+                        <th>状态</th>
+                    </tr>
                     </thead>
                     <tbody className=' '>
-                        {
-                            Array.from({ length: 10 }, (_, index) => (
-                                <Tooltip title="点击查看详情" color="purple" >
-                                    <tr className='*:text-center   hover:scale-y-150 hover:bg-blue-600  transitio-all duration-100' onClick={onReviewDetail}>
-                                        <td >
-                                            2022-01-20
-                                        </td>
-                                        <td>
-                                            病假
-                                        </td>
-                                        <td>
-                                            已通过{index}
-                                        </td>
-                                    </tr>
-                                </Tooltip>
-                            ))
-                        }
+                    {
+                        leaveInfos.map((item) => (
+                            <Tooltip title="点击查看详情" color="purple">
+                                <tr className='*:text-center   hover:scale-y-150 hover:bg-blue-600  transitio-all duration-100'
+                                    onClick={() => onReviewDetail(item.id)}>
+                                    <td>
+                                        {item.startTime}
+                                    </td>
+                                    <td>
+                                        {item.type}
+                                    </td>
+                                    <td>
+                                        {item.status===2?"通过":(item.status===3?"未通过":"待审核")}
+                                    </td>
+                                </tr>
+                            </Tooltip>
+                        ))
+                    }
                     </tbody>
                 </table>
-                <LeaveDetailDrawer visable={visable} onClose={onCloseDrawer} />
+                <LeaveDetailDrawer visable={visable} onClose={onCloseDrawer} index={itemIndex}/>
             </div>
             <div className='w-full flex justify-center items-center'>
                 <div className='w-full mt-[50px] flex justify-center items-center'>
-                    <Pagination showQuickJumper defaultCurrent={2} total={500} pageSize={20} onChange={onPageChange} responsive={true} />
+                    <Pagination showQuickJumper defaultCurrent={1} total={500} pageSize={pageSize} onChange={onPageChange}
+                                responsive={true} onShowSizeChange={onShowSizeChange}/>
                 </div>
             </div>
-        </div >
+        </div>
     )
 }
 export default LeaveList
